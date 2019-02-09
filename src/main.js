43ebc8a55ab0611d53dcfd4def5955defd580cba
Will Vincent <will@willvincent.com>
2019-02-08 23:03:25 -0600

    chore: Add treeselect and draggable-nested-tree packages & custom styles

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import App from './views/App.vue'
 import router from './router'
 import store from './store'
 
+import '@riophae/vue-treeselect/dist/vue-treeselect.css';
+
 Vue.config.productionTip = false
 
 Vue.use(Buefy)
